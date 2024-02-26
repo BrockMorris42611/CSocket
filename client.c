c859161b88e79dbb391ac1cc30b73907e8225699
BrockMorris42611 <brock36morris@gmail.com>
2024-02-26 08:14:54 -0500

    very very crude implementation of a multithreaded server chat. Just testing my understanding of socket() and accept() on the server side.

--- client.c
+++ client.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+
+/*for sockets*/
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <sys/fcntl.h>
 #include <netdb.h>
+
+/*for threading*/
+#include <sys/fcntl.h>
+#include <pthread.h>
+
 //bryan and ohallarant pg 953
-
-
 int main(){
-struct sockaddr_in saddr;
+    struct sockaddr_in saddr;
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(0x50); //port 80 in hex (in network byte order)
     //                      localhost:127.0.0.1
@@ -23,6 +26,7 @@ struct sockaddr_in saddr;
         perror("ERROR creating server fd");
         return EXIT_FAILURE;
     }
+    printf("sock: %d", clientfd);
     int conn = connect(clientfd, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
     if(conn == -1){
         perror("ERROR creating listener");
--- server.c
+++ server.c
@@ -1,56 +1,81 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+/*for sockets*/
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <sys/fcntl.h>
 #include <netdb.h> // Include netdb.h for getaddrinfo()
-#include <errno.h>
+
+/*for threading*/
+#include <sys/fcntl.h>
+#include <pthread.h>
+
 //bryan and ohallarant pg 953
-//edit c/cpp extension to have cmd argument "std=gnu17"
+//edit c/cpp extension to have cmd argument "-std=gnu17"
+void *begin_talking(void *__fd);
+int NUM_OF_THREADS = 2;
+
 int main(){
+    pthread_t talking_thread;
+    pthread_t talking_thread0;
+    //int file_desc_arr[2];
+    int serverfd, binder, listener, accepted, accepted0;
 
-    struct sockaddr_in saddr;
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(0x50); //port 80 in hex (host -> network byte order)
-    //                       localhost:127.0.0.1
-    //                          7F=127|00=0|00=0|001=1
-    saddr.sin_addr.s_addr = htonl(0x7F000001);//localhost in hex (host -> network byte order)
-    int serverfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(serverfd == -1){
-        perror("ERROR creating server fd");
+    struct sockaddr_in saddr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(0x50), //Port 80 in hex (host->network byte order)
+        .sin_addr.s_addr = htonl(0x7F000001) //Localhost: 127.0.0.1 in hex (host->network byte order)
+    };
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(serverfd == -1){perror("ERROR creating server fd");
         return EXIT_FAILURE;
     }
-    int binder = bind(serverfd, (struct sockaddr*) &saddr, sizeof(struct sockaddr_in));
-    if(binder == -1){
-        perror("ERROR creating binder");
+    binder = bind(serverfd, (struct sockaddr*) &saddr, sizeof(struct sockaddr_in));
+    if(binder == -1){perror("ERROR creating binder");
         return EXIT_FAILURE;
     }
-    int listener = listen(serverfd, 1024);
-    if(listener == -1){
-        perror("ERROR creating listener");
+    listener = listen(serverfd, 1024);
+    if(listener == -1){perror("ERROR creating listener");
         return EXIT_FAILURE;
     }
-    int accepted = accept(serverfd, (struct sockaddr*) &saddr, &(socklen_t){sizeof(saddr)});
-
-    if(accepted == -1){
-        perror("ERROR creating accepted");
-        return EXIT_FAILURE;
+    int i = 0;
+    while(1){
+        if(!i){
+            accepted = accept(serverfd, (struct sockaddr*) &saddr, &(socklen_t){sizeof(saddr)});
+            if(accepted == -1){perror("ERROR creating accepted");
+                return EXIT_FAILURE;
+            }
+            printf("-sock: %d\n instance:%d\n", serverfd, accepted);
+            pthread_create(&talking_thread, NULL, begin_talking, &accepted);
+        }else{
+            accepted0 = accept(serverfd, (struct sockaddr*) &saddr, &(socklen_t){sizeof(saddr)});
+            if(accepted0 == -1){perror("ERROR creating accepted");
+                return EXIT_FAILURE;
+            }
+            printf("*sock: %d\n instance:%d\n", serverfd, accepted0);
+            pthread_create(&talking_thread0, NULL, begin_talking, &accepted0);
+        }
     }
-
-    char hello[128];
-    ssize_t bytes_recv;
-    while((bytes_recv = recv(accepted, hello, sizeof(hello), 0)) > 0){
+    
+    pthread_join(talking_thread, NULL);
+    pthread_join(talking_thread0, NULL);
+    close(accepted); // Close the accepted socket after the loop
+    return EXIT_SUCCESS;
+}
+void *begin_talking(void *fd){
+    int sock_fd = *(int*)fd;
+    char hello[128]; ssize_t bytes_recv;
+    while((bytes_recv = recv(sock_fd, hello, sizeof(hello), 0)) > 0){
         printf("from client> %s", hello);
     }
     if (bytes_recv == -1) {
         perror("ERROR receiving data");
-        close(accepted); // Close the accepted socket on error
-        return EXIT_FAILURE;
+        close(sock_fd); // Close the accepted socket on error
+        return (void *)EXIT_FAILURE;
     }
-    close(accepted); // Close the accepted socket after the loop
-    return EXIT_SUCCESS;
+    return (void *)EXIT_SUCCESS;
 }
